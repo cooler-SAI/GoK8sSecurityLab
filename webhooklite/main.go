7ba630cbbcd8257c86d99e5fefa42a51aec8bb10
cooler-SAI <coolerme86@gmail.com>
2026-03-05 19:17:49 +0300

    feat(webhook): Implement basic HTTP server with logging for webhook requests

--- webhooklite/main.go
+++ webhooklite/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello tests")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		bytesWritten, err := fmt.Fprintf(w, "hello webhook")
+		if err != nil {
+			log.Println("Error writing response:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Request received: %s, bytes written: %d", r.URL.Path, bytesWritten)
+	})
+
+	port := "8080"
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
